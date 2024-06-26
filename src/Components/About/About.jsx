92f8a9e5502dd51e5f1f3a3c255517cf4a91d9e7
Mohau Mushi <david1mushi1@gmail.com>
2024-06-26 21:53:00 +0200

    Updated About component CSS and JSX

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
-import './About.css';
+import React from "react";
+import AboutImage from "../../assets/Image.jpg";
+import "./About.css";
 
 const About = () => {
   return (
     <section id="about" className="about-section">
-      <h1>About Us</h1>
-      <p>Learn more about our gym, our mission, and our values.</p>
+      <div className="about-content">
+        <h1>About Us</h1>
+        <div className="about-container">
+          <div className="image-container">
+            <img src={AboutImage} alt="About Me" className="about-image" />
+          </div>
+          <div className="text-container">
+            <p>Learn more about our gym, our mission, and our values.</p>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
