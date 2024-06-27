3a5b4c631035525c94ea32c437c06ff9809c354b
Mohau Mushi <david1mushi1@gmail.com>
2024-06-27 14:28:21 +0200

    Refactored Services component CSS and JSX to include a services grid with cards, updated styling and layout

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,11 +1,54 @@
 import React from 'react';
-import './Services.css';
+import "./Services.css";
+
+const servicesData = [
+  {
+    title: "One-on-One Personal Training",
+    description: "Get personalized attention and customized workouts tailored to your specific goals and needs.",
+    image: "/src/assets/one-on-one.jpg"
+  },
+  {
+    title: "Small Group Training",
+    description: "Enjoy the benefits of personal training in a motivating group setting with like-minded individuals.",
+    image: "/src/assets/Small-Group-Training.jpg"
+  },
+  {
+    title: "Online Coaching",
+    description: "Receive expert guidance and support from anywhere, with flexible programs that fit your schedule.",
+    image: "/src/assets/Online-Coaching.jpg"
+  },
+  {
+    title: "Specialized Programs",
+    description: "Target specific goals with our specialized programs, including weight loss, muscle gain, and sports performance.",
+    image: "/src/assets/Specialized-Programs.jpg"
+  },
+  {
+    title: "Corporate Wellness Programs",
+    description: "Boost employee health and productivity with our tailored corporate wellness solutions.",
+    image: "/src/assets/Corporate-Wellness-Programs.jpg"
+  },
+  {
+    title: "Fitness Workshops and Seminars",
+    description: "Expand your knowledge and skills with our educational workshops and seminars on various fitness topics.",
+    image: "/src/assets/Fitness Workshops and Seminars.jpg"
+  }
+];
 
 const Services = () => {
   return (
     <section id="services" className="services-section">
-      <h1>Our Services</h1>
-      <p>Discover the wide range of services we offer to help you achieve your fitness goals.</p>
+      <h1>Services</h1>
+      <p className="services-description">Discover the wide range of services i offer to help you achieve your fitness goals.</p>
+      <div className="services-grid">
+        {servicesData.map((service, index) => (
+          <div key={index} className="service-card">
+            <img src={service.image} alt={service.title} className="service-image" />
+            <h2 className="service-title">{service.title}</h2>
+            <p className="service-description">{service.description}</p>
+            {/* <button className="learn-more-btn">Learn More</button> */}
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
